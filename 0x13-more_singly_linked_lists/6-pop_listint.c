6d810af8793a1ba8aaf4056b5cd2c60d1befeeb9
MohammedTalhi <talhi.mohammed@gmail.com>
2023-05-02 15:20:56 -0700

    x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * deletes the head node of a linked list  pointing to the first node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
+
+
